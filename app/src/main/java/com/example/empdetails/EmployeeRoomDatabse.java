135d21639d7654b61cc11fbcc9ea29462d559647
Mehadi <mehadihasan@iut-dhaka.edu>
2019-02-13 18:37:53 +0600

    Initial push

--- app/src/main/java/com/example/empdetails/EmployeeRoomDatabse.java
+++ app/src/main/java/com/example/empdetails/EmployeeRoomDatabse.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
-@Database(entities = {Employee.class}, version = 1)
+@Database(entities = {Employee.class}, version = 1, exportSchema = false)
 public abstract class EmployeeRoomDatabse extends RoomDatabase {
 
     public abstract EmployeeDao employeeDao();
